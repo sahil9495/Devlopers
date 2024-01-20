42cf4a3f832ae297dbb6eb64d48dbe7182bc1b5e
unknown <sahiltamboli9494@gmail.com>
2024-01-20 16:58:53 +0530

    Front Page

---
+++ index.js
@@ -0,0 +1,9 @@
+// Open the pop-up box when the page loads
+window.onload = function () {
+    document.getElementById('popup-box').style.display = 'block';
+};
+
+// Close the pop-up box
+function closePopup() {
+    document.getElementById('popup-box').style.display = 'none';
+}
