8bdffef1784e4bec82e1edc9d23646e57391d9d7
unknown <sahiltamboli9494@gmail.com>
2024-01-26 23:25:51 +0530

    internship section

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Open the pop-up box when the page loads
+var value = document.getElementById("home");
 window.onload = function () {
     document.getElementById('popup-box').style.display = 'block';
+   document.getElementById('home').style.filter= 'blur(10px)';
 };
 
 // Close the pop-up box
 function closePopup() {
     document.getElementById('popup-box').style.display = 'none';
+    document.getElementById('home').style.filter= 'blur(0px)';
 }
